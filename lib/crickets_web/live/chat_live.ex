093b9ba1fa125ac0c2e63dd2629397eefa46c360
Jatin Sonavane <jatins@hey.com>
2023-09-26 09:19:57 -0700

    Creates structure of chat window

--- lib/crickets_web/live/chat_live.ex
+++ lib/crickets_web/live/chat_live.ex
@@ -13,17 +13,25 @@ defmodule CricketsWeb.ChatLive do
 
   def render(assigns) do
     ~H"""
-    <div>hi <%= @current_user.email %>!</div>
-    <div class="flex">
-      <div>
-      <ol>
-        <li>Coffee</li>
-        <li>Tea</li>
-        <li>Milk</li>
-      </ol>
+    <%!-- <div>hi <%= @current_user.email %>!</div> --%>
+    <%!-- Main container --%>
+    <div class="chat-container">
+      <%!-- Friends --%>
+      <div class="friends-container">
       </div>
-      <div>
-
+      <%!-- Message Header --%>
+      <div class="msg-container">
+        <%!-- Who you're talking to --%>
+        <div class="msg-header">
+        </div>
+        <%!-- Conversations --%>
+        <div class="msg-page">
+        </div>
+        <%!-- Message input --%>
+        <div class="msg-input-container">
+          <textarea class="msg-input" />
+          <button>Send</button>
+        </div>
       </div>
     </div>
     """
