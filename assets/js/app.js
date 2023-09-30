868c33f25ff44f216a97fb8fabdd08a5908a1480
Jatin Sonavane <jatins@hey.com>
2023-09-30 11:19:35 -0700

    Cleans up code and adds chat refreshing

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,10 +26,32 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 
 let Hooks = {}
 Hooks.Focus = {
-  mounted() {
-    this.el.focus();
-  }
+    mounted() {
+        this.el.focus();
+    }
 }
+
+Hooks.LocalStateStore = {
+    mounted() {
+        this.handleEvent("store", (obj) => this.store(obj))
+        this.handleEvent("clear", (obj) => this.clear(obj))
+        this.handleEvent("restore", (obj) => this.restore(obj))
+    },
+
+    store(obj) {
+        localStorage.setItem(obj.key, obj.data)
+    },
+
+    restore(obj) {
+        var data = localStorage.getItem(obj.key)
+        this.pushEvent(obj.event, data)
+    },
+
+    clear(obj) {
+        localStorage.removeItem(obj.key)
+    }
+}
+
 
 let liveSocket = new LiveSocket(
     "/live", 
--- lib/crickets_web/live/chat_live.ex
+++ lib/crickets_web/live/chat_live.ex
@@ -1,14 +1,28 @@
 defmodule CricketsWeb.ChatLive do
+  @moduledoc """
+  This is the live component powering chat functionality.
+  At a high level, this functionality encompasses:
+  * Seeing which friends are currently online
+  * Chatting with friends
+  * Saving chats to local storage to preserve continuity without storing chat messages on the backend.
+  """
+
+  require Logger
   use CricketsWeb, :live_view
 
-  alias CricketsWeb.Presence
-  alias Crickets.ChatMessage
+  alias CricketsWeb.Presence # used to track which friends are online
+  alias Crickets.ChatMessage # named struct holding a single chat message
 
   # Topic to track which users are online
   @online_users_presence_topic "crickets:online_users_presence_topic"
 
+
+  @doc """
+  Invoked when the user either comes to `/chat` for the first time, or refreshes the page, or if the backend restarts.
+  """
+  @impl true
   def mount(_params, _session, socket) do
-    me = socket.assigns.current_user.handle || socket.assigns.current_user.email
+    me = socket.assigns.current_user.handle
 
     # Track me so that my friends can see when I'm online.
     Presence.track(
@@ -18,7 +32,7 @@ defmodule CricketsWeb.ChatLive do
       %{}
     )
 
-    # Interested in messages directed at me. Each user has a topic named after his/her email.
+    # Interested in messages directed at me. Each user has a topic named after his/her handle.
     CricketsWeb.Endpoint.subscribe(me)
     # Interested in getting updates about which of my friends are online.
     CricketsWeb.Endpoint.subscribe(@online_users_presence_topic)
@@ -35,6 +49,28 @@ defmodule CricketsWeb.ChatLive do
     }
   end
 
+
+  @doc """
+  Requesting the browser for the previous state to restore from.
+  See: https://fly.io/phoenix-files/saving-and-restoring-liveview-state/
+  """
+  @impl true
+  def handle_params(_params, _, socket) do
+    # Only try to talk to the client when the websocket is setup. Not on the initial "static" render.
+    new_socket =
+      if connected?(socket) do
+        socket
+        # request the browser to restore any state it has for me.
+        |> push_event("restore", %{key: socket.assigns.me, event: "restoreSettings"})
+      else
+        socket
+      end
+
+    {:noreply, new_socket}
+  end
+
+
+  @impl true
   def render(assigns) do
     ~H"""
     <%!-- Main container --%>
@@ -108,7 +144,9 @@ defmodule CricketsWeb.ChatLive do
     """
   end
 
-  # Called when user tries to send a message.
+
+  # Invoked when the user hits `Enter` in the chat box.
+  @impl true
   def handle_event("send-message", %{"shiftKey" => isShiftKeyPressed, "value" => message}, socket) do
     # Shift + Enter allows the user to put a newline in the message.
     if !isShiftKeyPressed do
@@ -119,11 +157,8 @@ defmodule CricketsWeb.ChatLive do
         :at => DateTime.utc_now()
       }
 
-      CricketsWeb.Endpoint.broadcast!(
-        chat_message.to,
-        "new_msg",
-        Jason.encode!(chat_message)
-      )
+      # Send the chat out to the recipient
+      CricketsWeb.Endpoint.broadcast!(chat_message.to, "chat", Jason.encode!(chat_message))
 
       if chat_message.from != chat_message.to do
         {
@@ -145,6 +180,8 @@ defmodule CricketsWeb.ChatLive do
     end
   end
 
+
+  # Invoked when the user clicks on a friend.
   def handle_event("friend-clicked", %{"friend-name" => friend}, socket) do
     {
       :noreply,
@@ -154,29 +191,66 @@ defmodule CricketsWeb.ChatLive do
     }
   end
 
-  def handle_info(%Phoenix.Socket.Broadcast{event: "new_msg", payload: payload}, socket) do
+
+  # Sent by the JS hook `LocalStateStore` in response to the server having asked to restore state.
+  # See: https://fly.io/phoenix-files/saving-and-restoring-liveview-state/
+  def handle_event("restoreSettings", token_data, socket) when is_binary(token_data) do
+    socket =
+      case restore_from_token(token_data) do
+        {:ok, nil} ->
+          # do nothing with the previous state
+          socket
+
+        {:ok, restored} ->
+          socket
+          |> assign(:chats, restored) # TODO actually merge the chats
+
+        {:error, reason} ->
+          # Clear the token so it doesn't keep showing an error.
+          socket
+          |> clear_browser_storage()
+      end
+
+    {:noreply, socket}
+  end
+
+
+  # Sent by the JS hook `LocalStateStore` in response to the server having asked to restore state.
+  # See: https://fly.io/phoenix-files/saving-and-restoring-liveview-state/
+  def handle_event("restoreSettings", _token_data, socket) do
+    # No expected token data received from the client
+    Logger.debug("No LiveView SessionStorage state to restore")
+    {:noreply, socket}
+  end
+
+
+  # Incoming chat message
+  @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "chat", payload: payload}, socket) do
     chat_message = Jason.decode!(payload)
-    chat_message = %ChatMessage{
-      from: Map.get(chat_message, "from"),
-      to: Map.get(chat_message, "to"),
-      message: Map.get(chat_message, "message"),
-      at: Map.get(chat_message, "at")
-    }
 
     {
       :noreply,
       socket
-      |> handle_new_chat_message(chat_message)
+      |> handle_new_chat_message(%ChatMessage{
+        from:     Map.get(chat_message, "from"),
+        to:       Map.get(chat_message, "to"),
+        message:  Map.get(chat_message, "message"),
+        at:       Map.get(chat_message, "at")
+      })
     }
   end
 
+
+  # There's been a change to who's online.
   def handle_info(%Phoenix.Socket.Broadcast{event: "presence_diff"}, socket) do
     {
       :noreply,
       socket
-      |> assign(:friends, Presence.list(@online_users_presence_topic)) # TODO narrow to friends
+      |> assign(:friends, Presence.list(@online_users_presence_topic)) # TODO this will get expensive as number of users increases
     }
   end
+
 
   defp handle_new_chat_message(socket, chat_message = %ChatMessage{}) do
     chatting_with = if(chat_message.to == socket.assigns.me, do: chat_message.from, else: chat_message.to)
@@ -193,7 +267,39 @@ defmodule CricketsWeb.ChatLive do
       socket.assigns.voicemail
     end
 
-    socket = update(socket, :chats, fn _ -> chats end)
-    update(socket, :voicemail, fn _ -> voicemail end)
+    socket
+    |> update(:chats, fn _ -> chats end)
+    |> update(:voicemail, fn _ -> voicemail end)
+    |> push_event("store", %{
+      key:  socket.assigns.me,
+      data: serialize_to_token(chats) # TODO we ought to version this data
+    }) # TODO ideally this goes into a liveview lifecycle hook just before the process is killed
+  end
+
+  defp restore_from_token(token) do
+    salt = Application.get_env(:crickets, CricketsWeb.Endpoint)[:live_view][:signing_salt]
+
+    # Max age is 7 days. 86,400 seconds * 7
+    # TODO we should really embed version number into the token
+    case Phoenix.Token.decrypt(CricketsWeb.Endpoint, salt, token, max_age: 86_400 * 7) do
+      {:ok, data} ->
+        {:ok, data}
+
+      {:error, reason} ->
+        # handles `:invalid`, `:expired` and possibly other things?
+        {:error, "Failed to restore previous state. Reason: #{inspect(reason)}."}
+    end
+  end
+
+
+  defp serialize_to_token(state_data) do
+    salt = Application.get_env(:crickets, CricketsWeb.Endpoint)[:live_view][:signing_salt]
+    Phoenix.Token.encrypt(CricketsWeb.Endpoint, salt, state_data)
+  end
+
+
+  # Push a websocket event down to the browser's JS hook to clear any chat data.
+  defp clear_browser_storage(socket) do
+    push_event(socket, "clear", %{key: socket.assigns.me})
   end
 end
